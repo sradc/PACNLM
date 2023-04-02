ba43f9394198b48c190b0e4f51e74b46b7096cfc
sradc <sidneyradcliffe@sky.com>
2023-04-02 09:27:53 +0100

    try adding check for uncommited stuff in train.sh

--- train.sh
+++ train.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+if [[ $(git status --porcelain) ]]; then
+    echo "There are staged changes or untracked files in Git. Aborting script."
+    exit 1
+fi
 export SAVEDIR="data/train_$(date +%Y%m%d_%H%M%S)"
 echo SAVEDIR=$SAVEDIR
 jupyter nbconvert main.ipynb --to script
